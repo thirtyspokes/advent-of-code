6a0e42daad5904b8febb21ae2d90e75a115ea628
Ray Ashman Jr <ray@rayashmanjr.com>
2015-12-29 18:42:43 -0500

    Day Five comments

--- src/advent_of_code/day_five/strings.clj
+++ src/advent_of_code/day_five/strings.clj
@@ -1,11 +1,15 @@
 (ns advent-of-code.day_five.strings)
 
 (defn has-three-vowels?
+  "Determines if the input string contains at least three
+  occurrences of any vowel."
   [input]
   (<= 3
       (count (filter #(contains? #{\a \e \i \o \u} %) input))))
 
 (defn has-repeated-letter?
+  "Returns true if the input string contains any instance
+  of the same letter repeated twice."
   [input]
   (let [[start & tail] input]
     (loop [letter start remaining tail]
@@ -15,10 +19,18 @@
             (recur (first remaining) (rest remaining))))))
 
 (defn has-no-bad-seqs?
+  "Returns true if the input string does not contain any of
+  the sequences 'ab', 'cd', 'pq', and 'xy'."
   [input]
   (nil? (re-find #"ab|cd|pq|xy" input)))
 
 (defn has-repeated-sequence?
+  "Returns true if the input string contains any two-letter sequence
+  of letters repeated twice, but not overlapping.
+
+  For example, 'abasdfab' returns true ('ab'), but 'aaaxda' will not
+  return true for the 'aa' sequence.  Adjacent but not overlapping
+  sequences are OK - 'abcdiiii' will return true, as will 'abcdiaia'."
   [input]
   (= true
      (reduce (fn [seen current]
@@ -32,12 +44,17 @@
              (partition 2 1 input))))
 
 (defn has-gapped-repeating-letter?
+  "Returns true if the input string has any instance of the same
+  letter repeated twice but separated by some other character, such
+  as 'axa'."
   [input]
   (seq
    (filter (fn [[a _ b]] (= a b))
            (partition 3 1 input))))
 
 (defn naughty-or-nice?
+  "A 'nice' string returns true for `has-three-vowels?`, 
+  `has-repeatedletter?`, and `has-no-bad-seqs?`."
   [input]
   (if (and (has-three-vowels? input)
             (has-repeated-letter? input)
@@ -46,6 +63,8 @@
     :naughty))
 
 (defn better-naughty-or-nice?
+  "A 'nice' string returns true for both `has-gapped-repeating-letter?`
+  and `has-repeated-sequence?`."
   [input]
   (if (and (has-gapped-repeating-letter? input)
            (has-repeated-sequence? input))
